136adfe085b0e696528455c5654e65d5bc2745bc
Yue Yu <yuyue9284@outlook.com>
2017-03-15 14:56:57 -0700

    ver 1.0 use setTimeout, 3secs

--- Weibo/test.js
+++ Weibo/test.js
@@ -3,9 +3,8 @@ jqry.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jqu
 document.head.appendChild(jqry);
 
 setTimeout(function() {
-    $("p").css("color", "red");
-    alert("finish");
-    var block = ["女权", "tf", "TF", "Tf", "魅族", "萨德", "王源", "易烊千玺", "王俊凯", "周杰伦"];
+    alert("Ready");
+    var block = ["女权", "tf", "TF", "Tf", "魅族", "萨德", "王源", "易烊千玺", "王俊凯", "周杰伦", "直男"];
 
     var lst = $(".default-content.txt-xl").map(function() {
         return $(this); });
@@ -15,6 +14,7 @@ setTimeout(function() {
             var content = lst[i].text();
             if (content.includes(block[j])) {
                 lst[i].parents().eq(1).css("display", "none");
+                // lst[i].css('color','red');
                 break;
             }
         }
@@ -22,20 +22,17 @@ setTimeout(function() {
 
     $(".card-list").bind('DOMNodeInserted', function(e) {
         var element = e.target;
-        if (element == undefined) {
-            return;
-        }
         var t = $(element).find(".default-content.txt-xl");
         if (t == undefined) {
             return;
         }
-        t.css("color", 'red');
         for (var j = block.length - 1; j >= 0; j--) {
             var content = t.text();
             if (content.includes(block[j])) {
                 t.parents().eq(1).css("display", "none");
+                // t.css("color","red");
                 break;
             }
         }
     });
-}, 5000);
+}, 3000);
