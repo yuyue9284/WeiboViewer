cdbcca08a03b6996b1c8677cd934933100289793
Yue Yu <yuyue9284@outlook.com>
2017-03-18 20:11:05 -0700

    增加使用本地缓存功能，关键词输入clear时清空缓存，输入show显示屏蔽列表

--- Weibo/test.js
+++ Weibo/test.js
@@ -4,39 +4,68 @@ document.head.appendChild(jqry);
 
 
 
-var block = ["女权", "tf", "TF", "Tf", "魅族", "萨德", "王源", "易烊千玺", "王俊凯", "周杰伦", "直男", "三生三世", "习近平", "乐天", "红米"];
+var blocklist = ["tf", "TF", "Tf", "王源", "易烊千玺", "王俊凯"];
 
+// 初始化
 function init_blocker() {
-    var button = $("<button class='item box-col' id = 'add_keywords'>关键词</button>\
-                   <button class='item box-col' id = 'Blocker'>Block!</button>");
+    if (localStorage.blocklist != undefined) {
+        blocklist = JSON.parse(localStorage.blocklist);
+    } else {
+        localStorage.blocklist = JSON.stringify(blocklist);
+    }
+    // 初始化按钮
+    var button = $("<span class='item box-col' id = 'add_keywords'>关键词</span>\
+                   <span class='item box-col' id = 'Blocker'>Block!</span>");
     var navbar = $(".home-sub-nav.layout-box");
     if (navbar.length == 0) {
         var tnav = $('<div class="home-sub-nav layout-box" data-node="homeSubNav"></div>');
         $('body').prepend(tnav);
     }
     $(".home-sub-nav.layout-box").append(button);
+
+    // 执行清理
+    fire_blocker();
+
+    // 绑定函数
     $("#Blocker").click(fire_blocker);
     $('#add_keywords').click(newKeywords);
-    $("#app").on('DOMNodeInserted', new_feed_handler);
-    $(".card-list").on('DOMNodeInserted', new_feed_handler);
-
 }
 
+// 增加关键词
 function newKeywords() {
     var word = prompt("请输入");
-    block.push(word);
-    fire_blocker();
+    switch (word) {
+        case "show":
+            alert(blocklist);
+            break;
+        case "clear":
+            localStorage.removeItem('blocklist');
+            blocklist = [];
+            break;
+        default:
+            blocklist.push(word);
+            localStorage.blocklist = JSON.stringify(blocklist);
+            fire_blocker();
+    }
 }
 
-// 首页
+// 执行blocker
+function fire_blocker() {
+    hot_weibo_blocker();
+    index_blocker();
+    $("#app").off('DOMNodeInserted').on('DOMNodeInserted', new_feed_handler);
+    $(".card-list").off('DOMNodeInserted').on('DOMNodeInserted', new_feed_handler);
+}
+
+// 处理首页
 function index_blocker() {
     var lst = $(".default-content.txt-xl").map(function() {
         return $(this);
     });
     for (var i = lst.length - 1; i >= 0; i--) {
-        for (var j = block.length - 1; j >= 0; j--) {
+        for (var j = blocklist.length - 1; j >= 0; j--) {
             var content = lst[i].text();
-            if (content.includes(block[j])) {
+            if (content.includes(blocklist[j])) {
                 lst[i].parents().eq(1).css("display", "none");
                 // lst[i].css('color', 'red');
                 break;
@@ -46,16 +75,15 @@ function index_blocker() {
 
 }
 
-// 热门微博
+// 处理热门微博
 function hot_weibo_blocker() {
     var lst = $(".weibo-text").map(function() {
         return $(this);
     });
-
     for (var i = lst.length - 1; i >= 0; i--) {
-        for (var j = block.length - 1; j >= 0; j--) {
+        for (var j = blocklist.length - 1; j >= 0; j--) {
             var content = lst[i].text();
-            if (content.includes(block[j])) {
+            if (content.includes(blocklist[j])) {
                 lst[i].parents().eq(4).css("display", "none");
                 // lst[i].css('color', 'red');
                 break;
@@ -64,23 +92,20 @@ function hot_weibo_blocker() {
     }
 }
 
-//手动执行block
-function fire_blocker() {
-    hot_weibo_blocker();
-    index_blocker();
-    $("#app").off('DOMNodeInserted').on('DOMNodeInserted', new_feed_handler);
-    $(".card-list").off('DOMNodeInserted').on('DOMNodeInserted', new_feed_handler);
-}
 
-//处理新载入的内容
+// 处理新载入的内容
 function new_feed_handler(e) {
+
+    alert("launch!");
+
     var element = e.target;
     var t = $(element).find(".weibo-text");
     if (t != undefined) {
-        for (var j = block.length - 1; j >= 0; j--) {
+        for (var j = blocklist.length - 1; j >= 0; j--) {
             var content = t.text();
-            if (content.includes(block[j])) {
+            if (content.includes(blocklist[j])) {
                 t.parents().eq(4).css("display", "none");
+
                 // t.css("color", "red");
                 return;
             }
@@ -89,10 +114,11 @@ function new_feed_handler(e) {
 
     var t = $(element).find(".default-content.txt-xl");
     if (t != undefined) {
-        for (var j = block.length - 1; j >= 0; j--) {
+        for (var j = blocklist.length - 1; j >= 0; j--) {
             var content = t.text();
-            if (content.includes(block[j])) {
+            if (content.includes(blocklist[j])) {
                 t.parents().eq(1).css("display", "none");
+
                 // t.css("color", "red");
                 return;
             }
@@ -100,37 +126,4 @@ function new_feed_handler(e) {
     }
 }
 
-setTimeout(init_blocker, 3000);
-
-
-
-// $("#app").on('DOMNodeInserted', function(e) {
-//     var element = e.target;
-//     var t = $(element).find(".weibo-text");
-//     if (t == undefined) {
-//         return;
-//     }
-//     for (var j = block.length - 1; j >= 0; j--) {
-//         var content = t.text();
-//         if (content.includes(block[j])) {
-//             // t.parents().eq(4).css("display", "none");
-//             t.css("color", "red");
-//             break;
-//         }
-//     }
-// });
-// $(".card-list").on('DOMNodeInserted', function(e) {
-//     var element = e.target;
-//     var t = $(element).find(".default-content.txt-xl");
-//     if (t == undefined) {
-//         return;
-//     }
-//     for (var j = block.length - 1; j >= 0; j--) {
-//         var content = t.text();
-//         if (content.includes(block[j])) {
-//             //t.parents().eq(1).css("display", "none");
-//             t.css("color", "red");
-//             break;
-//         }
-//     }
-// });
+setTimeout(init_blocker, 500);
